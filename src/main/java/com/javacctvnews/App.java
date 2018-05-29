c8366c0c1a143d949e547ab4afcb451fb07da9ad
cyh <cyh@qq.com>
2018-05-29 23:49:04 +0800

    Initial commit

---
+++ src/main/java/com/javacctvnews/App.java
@@ -0,0 +1,13 @@
+package com.javacctvnews;
+
+/**
+ * Author：javacctvnews@163.com
+ * Create：2018/5/29 23:46
+ * Desc：
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
