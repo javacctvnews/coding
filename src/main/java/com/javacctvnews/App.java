0eb1e08dd1938c0b741aa2998a24b87ddc305032
cyh <cyh@qq.com>
2018-05-29 23:55:32 +0800

    1.字符串反转

--- src/main/java/com/javacctvnews/App.java
+++ src/main/java/com/javacctvnews/App.java
@@ -1,5 +1,7 @@
 package com.javacctvnews;
 
+import org.junit.Test;
+
 /**
  * Author：javacctvnews@163.com
  * Create：2018/5/29 23:46
@@ -10,4 +12,16 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    @Test
+    public void test001() {
+        String str = "abcdefg";
+        StringBuffer sb = new StringBuffer();
+        sb.append(str);
+        sb.reverse();
+        String reverseStr = sb.toString();
+        System.out.println(reverseStr);
+    }
+
+
 }
