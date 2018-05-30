7c7a67298eeb54a3aad7ba02f99fd4b5c013c299
javacctvnews <841132320@qq.com>
2018-05-30 18:03:57 +0800

    二进制转十六进制

--- src/main/java/com/javacctvnews/App.java
+++ src/main/java/com/javacctvnews/App.java
@@ -34,6 +34,17 @@ public class App {
     }
     
     
+    public static String parseByte2HexStr(byte buf[]) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < buf.length; i++) {
+			String hex = Integer.toHexString(buf[i] & 0xFF);
+			if (hex.length() == 1) {
+				hex = '0' + hex;
+			}
+			sb.append(hex.toUpperCase());
+		}
+		return sb.toString();
+	}
     
 
 
