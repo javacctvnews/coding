17bcd0d43c5bedfea00e4f378048b9bf88639576
javacctvnews <841132320@qq.com>
2018-05-31 11:42:44 +0800

    格式化代码

--- src/main/java/com/javacctvnews/CodingUtil.java
+++ src/main/java/com/javacctvnews/CodingUtil.java
@@ -9,41 +9,36 @@ import org.junit.Test;
  */
 
 public class CodingUtil {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 
-    @Test
-    public void test001() {
+	@Test
+	public void test001() {
 
-        String str = "abcdefg";
-        StringBuffer sb = new StringBuffer();
-        sb.append(str);
-        sb.reverse();
-        String reverseStr = sb.toString();
-        System.out.println(reverseStr);
-        int[] ints = test002();
-        System.out.println(ints);
-    }
-    
+		String str = "abcdefg";
+		StringBuffer sb = new StringBuffer();
+		sb.append(str);
+		sb.reverse();
+		String reverseStr = sb.toString();
+		System.out.println(reverseStr);
+		int[] ints = test002();
+		System.out.println(ints);
+	}
 
-
-    public int[] test002() {
-    	int[] arr = {2,3,7,4,6,6};
-    	for(int i=0;i<arr.length;i++){
-    		for (int j = i+1; j < arr.length-i; j++) {
-				if(arr[i]+arr[j]==9){
-					return new int[]{i,j};
+	public int[] test002() {
+		int[] arr = { 2, 3, 7, 4, 6, 6 };
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length - i; j++) {
+				if (arr[i] + arr[j] == 9) {
+					return new int[] { i, j };
 				}
 			}
-    	}
-    	return null;
-    }
-    
-    
-    
-    
-    public static String parseByte2HexStr(byte buf[]) {
+		}
+		return null;
+	}
+
+	public static String parseByte2HexStr(byte buf[]) {
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < buf.length; i++) {
 			String hex = Integer.toHexString(buf[i] & 0xFF);
@@ -54,7 +49,5 @@ public class CodingUtil {
 		}
 		return sb.toString();
 	}
-    
-
 
 }
